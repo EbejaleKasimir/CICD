0f169c6fb10d2430533420f646c8c88116846c67
EbejaleKasimir <kasimir.ikuenobe@gmail.com>
2023-08-30 17:27:52 +0100

    Update 30.08.2023

--- 9.  future tasks.cloud computing, capstone, and IT projects/Capston Project/Project 1/Project_1_query.sql
+++ 9.  future tasks.cloud computing, capstone, and IT projects/Capston Project/Project 1/Project_1_query.sql
@@ -1,14 +1,18 @@
-    SELECT SUM("Confirmed"), SUM("Deaths"), SUM("Recovered")
+    SELECT SUM("Confirmed") AS Total_Confirmed, 
+           SUM("Deaths") AS Total_Deaths, 
+           SUM("Recovered") AS Total_Recoveries
     FROM covid_19_data;
     
-    
+    -- There are four 4 quarters in a year. To capture the first quarter, we would have to look at the first three months.
     SELECT EXTRACT(YEAR FROM "ObservationDate") AS Year, 
-           SUM("Confirmed"), SUM("Deaths"), SUM("Recovered")
+           SUM("Confirmed") AS Total_Confirmed, 
+           SUM("Deaths") AS Total_Deaths, 
+           SUM("Recovered") AS Total_Recoveries
     FROM covid_19_data
     WHERE EXTRACT(MONTH FROM "ObservationDate") BETWEEN 1 AND 3
     GROUP BY Year;
     
-    
+    -- Comprehensive summary for each country captured in the table available in covid_19_data.csv file.
     SELECT "Country", 
            SUM("Confirmed") AS Total_Confirmed, 
            SUM("Deaths") AS Total_Deaths, 
@@ -16,8 +20,7 @@
     FROM covid_19_data
     GROUP BY "Country";
     
-    
-
+-- Percentage death increase from the year 2019 and 2020
     WITH "Deaths" AS (
       SELECT EXTRACT(YEAR FROM "ObservationDate") AS Year, 
              SUM("Deaths") AS Total_Deaths
@@ -30,15 +33,14 @@
     WHERE d1.Year = 2019 AND d2.Year = 2020;
     
 
-    
+-- Top Five 5 countries with the highest confirmed cases    
     SELECT "Country", SUM("Confirmed") AS Total_Confirmed
     FROM covid_19_data
     GROUP BY "Country"
     ORDER BY Total_Confirmed DESC
     LIMIT 5;
     
-
-    
+-- Net Change in confirmed cases per month over two-year period
     SELECT EXTRACT(YEAR FROM "ObservationDate") AS Year, 
            EXTRACT(MONTH FROM "ObservationDate") AS Month, 
            SUM("Confirmed") - LAG(SUM("Confirmed")) OVER (ORDER BY EXTRACT(YEAR FROM "ObservationDate"), EXTRACT(MONTH FROM "ObservationDate")) AS Net_Change
--- 9.  future tasks.cloud computing, capstone, and IT projects/Capston Project/Project 2/project_2_query.sql
+++ 9.  future tasks.cloud computing, capstone, and IT projects/Capston Project/Project 2/project_2_query.sql
@@ -13,19 +13,42 @@
     END;
     $$ LANGUAGE plpgsql;
 
+ALTER TABLE wpi_data_tbl
+    ALTER COLUMN "Latitude_degrees" TYPE bigint,
+    ALTER COLUMN "Longitude_degrees" TYPE bigint;
+	
+
+    CREATE OR REPLACE FUNCTION haversine(
+    lat1 float,
+    lon1 float,
+    lat2 float,
+    lon2 float
+    ) RETURNS float AS $$
+    DECLARE
+    x float := 69.1 * (lat2 - lat1);
+    y float := 69.1 * (lon2 - lon1) * cos(radians(lat1));
+    BEGIN
+    RETURN sqrt(x^2 + y^2) * 1.609344;
+    END;
+    $$ LANGUAGE plpgsql;
+
     SELECT
-    "Main_port_name" AS port_name,
-    haversine(CAST("Latitude_degrees" AS float),
-    CAST("Longitude_degrees" AS float),
-    CAST(1.0 AS float),
-    CAST(1.0 AS float)) AS distance_in_kilometers
+        "Main_port_name" AS port_name, "Latitude_degrees","Longitude_degrees",
+        haversine(CAST("Latitude_degrees" AS float),
+        CAST("Longitude_degrees" AS float),
+        -- Replace these values with a subquery that selects the latitude and longitude for JURONG ISLAND
+        (SELECT CAST("Latitude_degrees" AS float) FROM wpi_data_tbl WHERE "Main_port_name" = 'JURONG ISLAND'),
+        (SELECT CAST("Longitude_degrees" AS float) FROM wpi_data_tbl WHERE "Main_port_name" = 'JURONG ISLAND')) AS distance_in_kilometers
     FROM
-    wpi_data_tbl
+        wpi_data_tbl
     WHERE
-    "Main_port_name" <> 'JURONG ISLAND'
+        "Main_port_name" <> 'JURONG ISLAND'
     ORDER BY
-    distance_in_kilometers
+        distance_in_kilometers
     LIMIT 5;
+
+
+
 
     SELECT
     "Wpi_country_code" AS country,
@@ -40,22 +63,22 @@
     port_count DESC
     LIMIT 1;
 
-    SELECT
-    "Wpi_country_code" AS country,
-    "Main_port_name" AS port_name,
-    "Latitude_degrees" AS port_latitude,
-    "Longitude_degrees" AS port_longitude
-
+    SELECT 
+        "Wpi_country_code" AS country,
+        "Main_port_name" AS port_name, 
+        "Latitude_degrees" AS port_latitude,
+        "Longitude_degrees" AS port_longitude
     FROM
-    wpi_data_tbl
+        wpi_data_tbl
     WHERE
-    "Supplies_provisions" = 'Y' AND
-    "Supplies_water" = 'Y' AND
-    "Supplies_fuel_oil" = 'Y' AND
-    "Supplies_diesel_oil" = 'Y'
+        "Supplies_provisions" = 'Y' AND 
+        "Supplies_water" = 'Y' AND
+        "Supplies_fuel_oil" = 'Y' AND
+        "Supplies_diesel_oil" = 'Y'
     ORDER BY
-    (haversine(CAST("Latitude_degrees" AS float),
-    CAST("Longitude_degrees" AS float),
-    CAST(1.0 AS float),
-    CAST(1.0 AS float)))
+        (haversine(CAST("Latitude_degrees" AS float),
+                CAST("Longitude_degrees" AS float), 
+                32.610982, 
+                -38.706256))
     LIMIT 1;
+
